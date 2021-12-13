4a207e18f3dc194c535c21a560824de3cbe46df1
Maslovskiy1 <95534023+Maslovskiy1@users.noreply.github.com>
2021-12-13 02:01:59 +0200

    Create eJS.js

---
+++ eJS.js
@@ -0,0 +1,7 @@
+send.addEventListener("click", fnc);
+function fnc(){
+    let val_a = a.value;
+    let val_b = b.value;
+    let val_c = a+b;
+    val_c = parseInt(val_a) + parseInt(val_b)
+    cc.value = val_c;
