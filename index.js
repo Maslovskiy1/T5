4cfc8433fc91b5dfed54cf54fb2afcb0341d71ce
Maslovskiy1 <95534023+Maslovskiy1@users.noreply.github.com>
2021-12-13 03:13:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function fnc(){
     let val_a = a.value;
     let val_b = b.value;
     let val_c = a + b;
-    val_C = parseInt(val_A) + parseInt(val_B);
+   let val_C = parseInt(val_A) + parseInt(val_B);
     CC.value = val_C;
 
 }
