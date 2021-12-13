1b7488154014cb3df832b540721b0a3158d7efb1
Maslovskiy1 <95534023+Maslovskiy1@users.noreply.github.com>
2021-12-13 03:09:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-btn.addEventListener("click",f_out);
+send.addEventListener("click",fnc);
+function fnc(){
+    let val_a = a.value;
+    let val_b = b.value;
+    let val_c = a + b;
+    val_Q = parseInt(val_M) / parseInt(val_P)* parseInt(val_V);
+    QQ.value = val_Q;
 
-   function f_out (){
-       c = Number(b.value) + Number(a.value);
-       res.innerHTML = c;
-   }
+}
